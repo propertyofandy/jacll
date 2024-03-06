374a668b7cfcb12524c9fb70211619ef51955c31
Anderson, Austin T <austinanderson246@gmail.com>
2024-03-05 21:33:49 -0800

    initail commit of project jacll with working linkage to llvm

---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+
+int main(){
+    std::cout << "hello world3" << std::endl;
+}
+
